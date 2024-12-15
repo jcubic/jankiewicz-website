efd6bb009f75c21ad63fec615ef1d4f34d913e26
Jakub Jankiewicz <jcubic@onet.pl>
2024-12-15 22:13:41 +0100

    add RSS feed generator

---
+++ rss/README.md
@@ -0,0 +1,24 @@
+# RSS Generator
+
+A generator that reads `../writing.php` file and create RSS file in `../rss.xml`.
+Code written by ChatGPT.
+
+## Installation
+```bash
+npm install
+```
+
+## Usage
+
+```bash
+./index.js
+```
+
+or
+
+```bash
+node ./index.js
+```
+
+## License
+Code released under [CC0](https://creativecommons.org/public-domain/cc0/) license
---
+++ rss/index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+const cheerio = require('cheerio');
+const RSS = require('rss');
+const fs = require('fs');
+
+(async () => {
+  try {
+    const html = fs.readFileSync('../writing.php', 'utf8');
+    const sanitizedHtml = html.replace(/<\?php.*?\?>/gs, '');
+
+    const $ = cheerio.load(sanitizedHtml);
+
+    const ul = $('article ul').first();
+
+    const feed = new RSS({
+      title: 'My Other Articles',
+      description: "Articles I've wrote for different publishers",
+      feed_url: 'https://jakub.jankiewicz.org/rss.xml',
+      site_url: 'https://jakub.jankiewicz.org/',
+      image_url: 'https://jakub.jankiewicz.org/favicon.ico',
+    });
+
+    ul.find('a').each((_, element) => {
+      const link = $(element).attr('href');
+      const title = $(element).text();
+
+      if (link && title) {
+        feed.item({
+          title,
+          url: link,
+          description: title,
+        });
+      }
+    });
+
+    fs.writeFileSync('../rss.xml', feed.xml({ indent: true }), 'utf8');
+  } catch (error) {
+    console.error('Error fetching or processing the page:', error.message);
+  }
+})();
