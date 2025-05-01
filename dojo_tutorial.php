61831dd747364b41434e1ecda396424733e823a9
Jakub Jankiewicz <jcubic@onet.pl>
2025-05-01 17:20:00 +0200

    update analytics to self-hosted Umami

--- dojo_tutorial.php
+++ dojo_tutorial.php
@@ -362,23 +362,8 @@ dojo.onLoad(function() {
     })();
     </script>
     <? if ($_SERVER["HTTP_HOST"] != "localhost" && !isset($_GET['track'])): ?>
-    <!-- Start Open Web Analytics Tracker -->
-    <script type="text/javascript">
-    //<![CDATA[
-    var owa_baseUrl = 'https://stats.jcubic.pl/';
-    var owa_cmds = owa_cmds || [];
-    owa_cmds.push(['setSiteId', 'b9a1a51b38e3837c9a5b309ec4122ff3']);
-    owa_cmds.push(['trackPageView']);
-    owa_cmds.push(['trackClicks']);
-    (function() {
-        var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
-        owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
-        _owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js';
-        var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
-    }());
-    //]]>
-    </script>
-    <!-- End Open Web Analytics Code -->
+      <script defer src="https://umami.jcubic.pl/script.js"
+              data-website-id="ecea8c54-de5d-43b6-8c68-11a91c8d0a24"></script>
     <? endif; ?>
     <p id="licence"><a rel="license" href="https://creativecommons.org/licenses/by-sa/2.5/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/2.5/80x15.png" /></a><br />This tutorial is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by-sa/2.5/">Creative Commons Attribution-ShareAlike 2.5 Generic License</a>, Copyright &copy; <?=date('Y')?> <a href="https://jakub.jankiewicz.org">Jakub T. Jankiewicz</a></p>
   </footer>
--- pl/footer.php
+++ pl/footer.php
@@ -17,22 +17,6 @@
         </div>
     </footer>
     <? if ($_SERVER["HTTP_HOST"] != "localhost" && !isset($_GET['track'])): ?>
-    <!-- Start Open Web Analytics Tracker -->
-    <script type="text/javascript">
-    //<![CDATA[
-    var owa_baseUrl = 'https://stats.jcubic.pl/';
-    var owa_cmds = owa_cmds || [];
-    owa_cmds.push(['setSiteId', 'b9a1a51b38e3837c9a5b309ec4122ff3']);
-    owa_cmds.push(['trackPageView']);
-    owa_cmds.push(['trackClicks']);
-    (function() {
-        var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
-        owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
-        _owa.src = owa_baseUrl + 'modules/base/js/owa.tracker-combined-min.js';
-        var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
-    }());
-    //]]>
-    </script>
-    <!-- End Open Web Analytics Code -->
-    <script defer src="https://cloud.umami.is/script.js" data-website-id="fc7d267c-fdc6-4b03-bfce-944828a2fe9b"></script>
+      <script defer src="https://umami.jcubic.pl/script.js"
+              data-website-id="ecea8c54-de5d-43b6-8c68-11a91c8d0a24"></script>
     <? endif; ?>
