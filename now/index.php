df90f7f7a7baf20cac502888b3c901a36e392e42
Jakub Jankiewicz <jcubic@onet.pl>
2025-12-09 23:42:35 +0100

    update /now page

--- now/index.php
+++ now/index.php
@@ -15,13 +15,13 @@
 <body id="tutorials">
     <?php include('../header.php'); ?>
     <article>
-        <h2>What I'm doing now (November 2025)</h2>
+        <h2>What I'm doing now (December 2025)</h2>
         <ul>
-          <li><strong>Working</strong>: Started working as a Senior Software Engineer at Sii Polska, working on my <a href="https://github.com/jcubic">Open Source projects</a>, mentoring people, volunteering for Polish Wikipedia (where I'm a trainer and a mentor for newcomers), and moderating Twitter communites.</li>
+          <li><strong>Working</strong>: as a Senior Software Engineer at Sii Polska, working on my <a href="https://github.com/jcubic">Open Source projects</a>, mentoring people, volunteering for Polish Wikipedia (where I'm a trainer and a mentor for newcomers), and moderating Twitter communites. I've also put a 2 day traning on WOŚP Charity about <a href="https://allegro.pl/oferta/2-dniowe-szkolenie-wikipedia-seo-i-nie-tylko-18136151156">Wikipedia + SEO</a> (Polish only)</li>
           <li><strong>Writing</strong>: The last two articles I've written were <a href="https://jakub.jankiewicz.org/blog/zig-book-pdf/">How to build zig book as PDF on Fedora?</a> and <a href="https://jakub.jankiewicz.org/blog/programming-advice/">My Advice to New Developers</a>.</li>
           <li><strong>Reading</strong>: Started reading a book "Essential Math for Data Science:" by Thomas Nield and "Quo vAIdis" by Andrzej Dragan.</li>
-          <li><strong>Listening</strong>: Recenlty enoying some <a href="https://en.wikipedia.org/wiki/Melodic_death_metal">Melodic Death Metal</a>. The last concert I've attended was <a href="https://en.wikipedia.org/wiki/Wardruna">Wardrina</a> (Ambient Pagan Folk from Norway).</li>
-          <li><strong>Watching</strong>: Recently watched <a href="https://en.wikipedia.org/wiki/There_Will_Be_Blood">There Will Be Blood</a> and <a href="https://en.wikipedia.org/wiki/F1_(film)">F1</a>.</li>
+          <li><strong>Listening</strong>: Recenlty enoying some <a href="https://en.wikipedia.org/wiki/Melodic_death_metal">Melodic Death Metal</a>. The last concert I've attended was <a href="https://en.wikipedia.org/wiki/Wardruna">Wardruna</a> (Ambient Pagan Folk from Norway).</li>
+          <li><strong>Watching</strong>: Recently re-watched <a href="https://en.wikipedia.org/wiki/Dopamine_(2003_film)">Dopamine</a>.</li>
         </ul>
         <p>This is a <a href="https://nownownow.com/about">now page</a> inspired by <a href="https://sive.rs/">Derek Sivers</a></p>
     </article>
