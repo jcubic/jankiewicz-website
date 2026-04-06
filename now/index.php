c51671c0e5e257bbb190718677e46718b255fd8f
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-04-06 14:50:37 +0200

    update /now page

--- now/index.php
+++ now/index.php
@@ -15,13 +15,13 @@
 <body id="tutorials">
     <?php include('../header.php'); ?>
     <article>
-        <h2>What I'm doing now (February 2026)</h2>
+        <h2>What I'm doing now (April 2026)</h2>
         <ul>
-          <li><strong>Working</strong>: I'm looking for a job as a Senior Software Engineer (Front-End focused), working on my <a href="https://github.com/jcubic">Open Source projects</a>, mentoring people, volunteering for Polish Wikipedia (where I'm a trainer and a mentor for newcomers), and moderating Twitter communites. I've also started <a href="https://jcubic.pl/wikipedia/">paid support for Wikipedia</a> and prering for the first traning that I've put on WOŚP Charity about "Wikipedia + SEO" (Polish only)</li>
-          <li><strong>Writing</strong>: The last article I've written was <a href="https://jakub.jankiewicz.org/blog/programming-advice/">My Advice to New Developers</a> that I share when someone ask on Twitter.</li>
-          <li><strong>Reading</strong>: Started reading a book "Essential Math for Data Science:" by Thomas Nield, "Quo vAIdis" by Andrzej Dragan, and "100 Ways to Improve your Writing" by Gary Provost.</li>
+          <li><strong>Working</strong>: I'm looking for a job as a Senior Software Engineer (Front-End focused), working on my <a href="https://github.com/jcubic">Open Source projects</a>, mentoring people, volunteering for Polish Wikipedia (where I'm a trainer and a mentor for newcomers), and moderating Twitter communites. I've also started <a href="https://jcubic.pl/wikizeit/oferta/">paid support for Wikipedia</a> and started a new <a href="https://jcubic.pl/wikizeit/">WikiZEIT project</a>, where I teach about Wikipedia and SEO (Polish only)</li>
+          <li><strong>Writing</strong>: The last article I've written was <a href="https://www.freecodecamp.org/news/how-to-create-a-table-of-contents-for-your-article/">How to Create a Table of Contents for Your Article</a> on FreeCodeCamp.</li>
+          <li><strong>Reading</strong>: Started reading a book "A Brief History of Intelligence" by Max Bennett</li>
           <li><strong>Listening</strong>: Recenlty listing playlist recommendation from YouTube. Also was able to download my favorite music from Spotify playlist using <a href="https://github.com/spotDL/spotify-downloader/">spotDL</a>.</li>
-          <li><strong>Watching</strong>: started watching <a href="https://lotr.fandom.com/wiki/The_Lord_of_the_Rings_Extended_Edition">Lord of The Rings (Special Extended Edition)</a> with all bonus materials.</li>
+          <li><strong>Watching</strong>: watched <a href="https://en.wikipedia.org/wiki/Project_Hail_Mary_(film)">Project Hail Mary</a> and started watching <a href="https://lotr.fandom.com/wiki/The_Lord_of_the_Rings_Extended_Edition">Lord of The Rings (Special Extended Edition)</a> with all bonus materials.</li>
         </ul>
         <p>This is a <a href="https://nownownow.com/about">now page</a> inspired by <a href="https://sive.rs/">Derek Sivers</a></p>
     </article>
