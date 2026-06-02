8d55b5f073060a47076af6bfc3cdb1d5ab3e885f
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-06-02 10:45:05 +0200

    new media pages

--- media.php
+++ media.php
@@ -7,15 +7,73 @@
     <?php include('head.php'); ?>
     <link rel="canonical" href="https://jakub.jankiewicz.org/video.php" />
 </head>
-<body id="tutorials">
-    <?php include('header.php'); ?>
-    <article>
-        <h2>Video recordings</h2>
-        <ul>
-          <li><a href="https://fosdem.org/2023/schedule/event/lipsscheme/">LIPS Scheme: Powerful introspection and extensibility (FOSDEM '23)</a></li>
-          <li><a href="https://youtu.be/3bdQZ67mpn8">ChatGPT w pomocy przy tworzeniu zapytań do Wikidata</a></li>
-        </ul>
-    </article>
-    <?php include('footer.php'); ?>
+<body id="projects">
+  <?php include('header.php'); ?>
+  <article>
+    <h2>Video recordings</h2>
+    <ul class="projects media">
+      <li itemscope itemtype="https://schema.org/VideoObject">
+        <a itemprop="url" href="https://fosdem.org/2023/schedule/event/lipsscheme/">
+          <img itemprop="thumbnailUrl" src="/images/jakub-jankiwicz-prezentacja-lips-scheme-fossdem.webp" alt="Presentation 'LIPS Scheme: Powerful introspection and extensibility' (FOSDEM '23) - Jakub Jankiewicz" />
+        </a>
+        <meta itemprop="name" content="LIPS Scheme: Powerful introspection and extensibility">
+        <meta itemprop="description" content="Presentation by Jakub Jankiewicz at FOSDEM 2023.">
+        <meta itemprop="uploadDate" content="2023-02-05">
+      </li>
+      <li itemscope itemtype="https://schema.org/VideoObject">
+        <a itemprop="url" href="https://youtu.be/3bdQZ67mpn8">
+          <img itemprop="thumbnailUrl" src="/images/jakub-jankiwicz-prezentacja-chatgpt-wikidane.webp" alt="Presentation: 'ChatGPT w pomocy przy tworzeniu zapytań do Wikidata' (ChatGPT in helping to create Wikidata queries) - Jakub Jankiewicz" />
+        </a>
+        <meta itemprop="name" content="ChatGPT w pomocy przy tworzeniu zapytań do Wikidata">
+        <meta itemprop="description" content="Presentation by Jakub Jankiewicz at the WZLOT 2024 conference.">
+        <meta itemprop="uploadDate" content="2024-06-08">
+      </li>
+    </ul>
+
+    <h2>My presentations at the WZLOT conference</h2>
+    <ul class="projects media">
+      <li itemscope itemtype="https://schema.org/ImageObject">
+        <img itemprop="contentUrl" src="/images/jakub-jankiewicz-wzlot-2023.webp" alt="Jakub T. Jankiewicz during presentation, sitting behind the laptop in WikiTrainer T-Shirt and visible arm tattoo"/>
+        <meta itemprop="caption" content="Jakub T. Jankiewicz - WZLOT 2023">
+        <p>
+          <span itemprop="about" itemscope itemtype="https://schema.org/Person">
+            <span itemprop="name">Jakub T. Jankiewicz</span>
+          </span>
+          during his first presentation at the WZLOT 2023 conference in Kraków entitled <a href="https://pl.wikimedia.org/wiki/Wzlot_2023/Program">'Wikikod nie jest straszny' (Wikicode is not scary)</a>.
+        </p>
+      </li>
+      <li itemscope itemtype="https://schema.org/ImageObject">
+        <img itemprop="contentUrl" src="/images/jakub-jankiewicz-wzlot-2024.webp" alt="Jakub T. Jankiewicz during presentation, sitting behind the laptop full of stickers, holding a microphone" />
+        <meta itemprop="caption" content="Jakub T. Jankiewicz - WZLOT 2024">
+        <p>
+          <span itemprop="about" itemscope itemtype="https://schema.org/Person">
+            <span itemprop="name">Jakub T. Jankiewicz</span>
+          </span>
+          during the presentation <a href="https://commons.wikimedia.org/wiki/File:Prezentacja_ChatGPT_i_Wikidane.pdf">'ChatGPT w pomocy przy tworzeniu zapytań do Wikidata'</a> at the WZLOT 2024 conference in Opole (the video recording is available above).
+        </p>
+      </li>
+      <li itemscope itemtype="https://schema.org/ImageObject">
+        <img itemprop="contentUrl" src="/images/jakub-jankiewicz-wzlot-2025.webp" alt="Jakub T. Jankiewicz during presentation, standing and pointing with a clicker in his hand"/>
+        <meta itemprop="caption" content="Jakub T. Jankiewicz - WZLOT 2025">
+        <p>
+          <span itemprop="about" itemscope itemtype="https://schema.org/Person">
+            <span itemprop="name">Jakub T. Jankiewicz</span>
+          </span>
+          during the presentation 'Jak działają szablony?' (How templates work?) at the national WZLOT conference in 2025 in Lublin. The presentation slides are available on <a href="https://commons.wikimedia.org/wiki/File:Prezentacja_szablony.pdf">Wikimedia Commons</a>. Based on this presentation, a new official help page was created: <a href="https://pl.wikipedia.org/wiki/Pomoc:Szablony">Pomoc:Szablony</a> on Polish Wikipedia.
+        </p>
+      </li>
+      <li itemscope itemtype="https://schema.org/ImageObject">
+        <img itemprop="contentUrl" src="/images/jakub-jankiewicz-wzlot-2026.webp" alt="Jakub T. Jankiewicz with a microphone"/>
+        <meta itemprop="caption" content="Jakub T. Jankiewicz - WZLOT 2026">
+        <p>
+          <span itemprop="about" itemscope itemtype="https://schema.org/Person">
+            <span itemprop="name">Jakub T. Jankiewicz</span>
+          </span>
+          during a discussion panel and presentation at the WZLOT 2026 conference in Łódź.
+        </p>
+      </li>
+    </ul>
+  </article>
+  <?php include('footer.php'); ?>
 </body>
 </html>
--- pl/media.php
+++ pl/media.php
@@ -8,14 +8,73 @@
     <link rel="canonical" href="https://jakub.jankiewicz.org/pl/video.php" />
 </head>
 <body id="tutorials">
-    <?php include('header.php'); ?>
-    <article>
-        <h2>Nagrania Video</h2>
-        <ul>
-          <li><a href="https://fosdem.org/2023/schedule/event/lipsscheme/">LIPS Scheme: Powerful introspection and extensibility (FOSDEM '23)</a></li>
-          <li><a href="https://youtu.be/3bdQZ67mpn8">ChatGPT w pomocy przy tworzeniu zapytań do Wikidata</a></li>
-        </ul>
-    </article>
-    <?php include('footer.php'); ?>
+  <?php include('header.php'); ?>
+  <article>
+    <h2>Nagrania wideo</h2>
+    <ul class="projects media">
+      <li itemscope itemtype="https://schema.org/VideoObject">
+        <a itemprop="url" href="https://fosdem.org/2023/schedule/event/lipsscheme/">
+          <img itemprop="thumbnailUrl" src="/images/jakub-jankiwicz-prezentacja-lips-scheme-fossdem.webp" alt="Prezentacja 'LIPS Scheme: Potężna introspekcja i rozszerzalność' (FOSDEM '23) - Jakub Jankiewicz" />
+        </a>
+        <meta itemprop="name" content="LIPS Scheme: Potężna introspekcja i rozszerzalność">
+        <meta itemprop="description" content="Prezentacja Jakuba Jankiewicza na konferencji FOSDEM 2023.">
+        <meta itemprop="uploadDate" content="2023-02-05">
+      </li>
+      <li itemscope itemtype="https://schema.org/VideoObject">
+        <a itemprop="url" href="https://youtu.be/3bdQZ67mpn8">
+          <img itemprop="thumbnailUrl" src="/images/jakub-jankiwicz-prezentacja-chatgpt-wikidane.webp" alt="Prezentacja: 'ChatGPT w pomocy przy tworzeniu zapytań do Wikidata' Jakub Jankiewicz" />
+        </a>
+        <meta itemprop="name" content="ChatGPT w pomocy przy tworzeniu zapytań do Wikidata">
+        <meta itemprop="description" content="Prezentacja Jakuba Jankiewicza na konferencji WZLOT 2024.">
+        <meta itemprop="uploadDate" content="2024-06-08">
+      </li>
+    </ul>
+
+    <h2>Moje wystąpienia na konferencji WZLOT</h2>
+    <ul class="projects media">
+      <li itemscope itemtype="https://schema.org/ImageObject">
+        <img itemprop="contentUrl" src="/images/jakub-jankiewicz-wzlot-2023.webp" alt="Jakub T. Jankiewicz podczas prezentacji, siedzący za laptopem w koszulce WikiTrenera z widocznym tatuażem na ramieniu"/>
+        <meta itemprop="caption" content="Jakub T. Jankiewicz - WZLOT 2023">
+        <p>
+          <span itemprop="about" itemscope itemtype="https://schema.org/Person">
+            <span itemprop="name">Jakub T. Jankiewicz</span>
+          </span>
+          podczas pierwszego wystąpienia na konferencji WZLOT 2023 w Krakowie pt. <a href="https://pl.wikimedia.org/wiki/Wzlot_2023/Program">„Wikikod nie jest straszny”</a>.
+        </p>
+      </li>
+      <li itemscope itemtype="https://schema.org/ImageObject">
+        <img itemprop="contentUrl" src="/images/jakub-jankiewicz-wzlot-2024.webp" alt="Jakub T. Jankiewicz podczas prezentacji, siedzący za laptopem pełnym naklejek, trzymający mikrofon" />
+        <meta itemprop="caption" content="Jakub T. Jankiewicz - WZLOT 2024">
+        <p>
+          <span itemprop="about" itemscope itemtype="https://schema.org/Person">
+            <span itemprop="name">Jakub T. Jankiewicz</span>
+          </span>
+          podczas prezentacji <a href="https://commons.wikimedia.org/wiki/File:Prezentacja_ChatGPT_i_Wikidane.pdf">„ChatGPT w pomocy przy tworzeniu zapytań do Wikidata”</a> na konferencji WZLOT 2024 w Opolu (nagranie wideo dostępne powyżej).
+        </p>
+      </li>
+      <li itemscope itemtype="https://schema.org/ImageObject">
+        <img itemprop="contentUrl" src="/images/jakub-jankiewicz-wzlot-2025.webp" alt="Jakub T. Jankiewicz podczas prezentacji, stojący i wskazujący klikerem trzymanym w dłoni"/>
+        <meta itemprop="caption" content="Jakub T. Jankiewicz - WZLOT 2025">
+        <p>
+          <span itemprop="about" itemscope itemtype="https://schema.org/Person">
+            <span itemprop="name">Jakub T. Jankiewicz</span>
+          </span>
+          podczas prezentacji „Jak działają szablony?” na ogólnopolskiej konferencji WZLOT w 2025 roku w Lublinie. Prezentacja jest dostępna na platformie <a href="https://commons.wikimedia.org/wiki/File:Prezentacja_szablony.pdf">Wikimedia Commons</a>. Na podstawie tego wystąpienia powstała oficjalna strona pomocy <a href="https://pl.wikipedia.org/wiki/Pomoc:Szablony">Pomoc:Szablony</a> w Polskiej Wikipedii.
+        </p>
+      </li>
+      <li itemscope itemtype="https://schema.org/ImageObject">
+        <img itemprop="contentUrl" src="/images/jakub-jankiewicz-wzlot-2026.webp" alt="Jakub T. Jankiewicz z mikrofonem"/>
+        <meta itemprop="caption" content="Jakub T. Jankiewicz - WZLOT 2026">
+        <p>
+          <span itemprop="about" itemscope itemtype="https://schema.org/Person">
+            <span itemprop="name">Jakub T. Jankiewicz</span>
+          </span>
+          podczas panelu dyskusyjnego i prelekcji w trakcie konferencji WZLOT 2026 w Łodzi.
+        </p>
+      </li>
+    </ul>
+  </article>
+
+  <?php include('footer.php'); ?>
 </body>
 </html>
