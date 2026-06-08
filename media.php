4029136570c86d9a1b330395682b4168b40f8eeb
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-06-08 22:51:38 +0200

    update title and description of media page

--- media.php
+++ media.php
@@ -2,8 +2,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>Jakub T. Jankiewicz - Video Presentations</title>
-    <meta name="Description" content="Jakub Jankiewicz - My Other Technical Articles"/>
+    <title>Jakub T. Jankiewicz - Media</title>
+    <meta name="Description" content="Jakub Jankiewicz - video and photographs"/>
     <?php include('head.php'); ?>
     <link rel="canonical" href="https://jakub.jankiewicz.org/video.php" />
 </head>
