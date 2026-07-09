dfd002c6d8fbcc005bb08dd3837cce7759601905
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-07-09 21:48:48 +0200

    update /now page

--- now/index.php
+++ now/index.php
@@ -15,13 +15,13 @@
 <body id="tutorials">
     <?php include('../header.php'); ?>
     <article>
-        <h2>What I'm doing now (June 2026)</h2>
+        <h2>What I'm doing now (July 2026)</h2>
         <ul>
-          <li><strong>Working</strong>: I'm still looking for a job as a Senior Software Engineer (Front-End focused), working on my <a href="https://github.com/jcubic">Open Source projects</a>, mentoring people, volunteering for Polish Wikipedia (where I'm a trainer and a mentor for newcomers), and moderating Twitter communites. I've also started <a href="https://jcubic.pl/wikizeit/oferta/">paid support for Wikipedia</a> and started a new <a href="https://wikizeit.edu.pl/">WikiZEIT project</a>, where I teach about Wikipedia and SEO (Polish only)</li>
-          <li><strong>Writing</strong>: The last article I've written was <a href="https://www.freecodecamp.org/news/how-to-create-a-table-of-contents-for-your-article/">How to Create a Table of Contents for Your Article</a> on FreeCodeCamp.</li>
-          <li><strong>Reading</strong>: Started reading a book "A Brief History of Intelligence" by Max Bennett</li>
-          <li><strong>Listening</strong>: Recenlty listing playlist recommendation from YouTube. Also was able to download my favorite music from Spotify playlist using <a href="https://github.com/spotDL/spotify-downloader/">spotDL</a>.</li>
-          <li><strong>Watching</strong>: watched <a href="https://en.wikipedia.org/wiki/Michael_(2026_film)">Michael</a>, <a href="https://en.wikipedia.org/wiki/Project_Hail_Mary_(film)">Project Hail Mary</a>, and started watching <a href="https://lotr.fandom.com/wiki/The_Lord_of_the_Rings_Extended_Edition">Lord of The Rings (Special Extended Edition)</a> with all bonus materials.</li>
+          <li><strong>Working</strong>: I'm still looking for a job as a Senior Software Engineer (Front-End focused, but thinking about pivoting to Full-stack with Python and AI), working on my <a href="https://github.com/jcubic">Open Source projects</a>, mentoring people, volunteering for Polish Wikipedia (where I'm a trainer and a mentor for newcomers), and moderating Twitter communites. Continue working on <a href="https://jcubic.pl/wikizeit/oferta/">paid support for Wikipedia and Wikidata</a> and my <a href="https://wikizeit.edu.pl/">WikiZEIT project</a>, where I teach about Wikipedia and SEO (Polish only)</li>
+          <li><strong>Writing</strong>: Wrote a few articles on FreeCodeCamp, WikiZEIT, and my personal blog (after a year of silence).</li>
+          <li><strong>Reading</strong>: Finished reading Polish book about personal brand by Ewelina Podrez-Siama.</li>
+          <li><strong>Listening</strong>: Recenlty Spotify playlist that I've downloaded using <a href="https://github.com/spotDL/spotify-downloader/">spotDL</a>.</li>
+          <li><strong>Watching</strong>: (don't watched anything new) last movies were <a href="https://en.wikipedia.org/wiki/Michael_(2026_film)">Michael</a>, <a href="https://en.wikipedia.org/wiki/Project_Hail_Mary_(film)">Project Hail Mary</a>, and started watching <a href="https://lotr.fandom.com/wiki/The_Lord_of_the_Rings_Extended_Edition">Lord of The Rings (Special Extended Edition)</a> with all bonus materials (didn't finish yet).</li>
         </ul>
         <p>This is a <a href="https://nownownow.com/about">now page</a> inspired by <a href="https://sive.rs/">Derek Sivers</a></p>
     </article>
