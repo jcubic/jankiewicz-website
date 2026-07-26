7b4c36e44a69f314418c242437f64999bd134246
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-07-26 23:15:57 +0200

    update /now page

--- now/index.php
+++ now/index.php
@@ -21,7 +21,7 @@
           <li><strong>Writing</strong>: Wrote a few articles on FreeCodeCamp, WikiZEIT, and my personal blog (after a year of silence).</li>
           <li><strong>Reading</strong>: Finished reading Polish book about personal brand by Ewelina Podrez-Siama. Started studying (after few months break) books about statistcs "Head First Statistcs", "Python for Data Analysis", and "Essential Math for Data Science"</li>
           <li><strong>Listening</strong>: Recenlty Spotify playlist that I've downloaded using <a href="https://github.com/spotDL/spotify-downloader/">spotDL</a>.</li>
-          <li><strong>Watching</strong>: (diding watch anything new) last movies were <a href="https://en.wikipedia.org/wiki/Michael_(2026_film)">Michael</a>, <a href="https://en.wikipedia.org/wiki/Project_Hail_Mary_(film)">Project Hail Mary</a>, and started watching <a href="https://lotr.fandom.com/wiki/The_Lord_of_the_Rings_Extended_Edition">Lord of The Rings (Special Extended Edition)</a> with all bonus materials (didn't finish yet).</li>
+          <li><strong>Watching</strong>: last movies were <a href="https://en.wikipedia.org/wiki/Michael_(2026_film)">Michael</a>, <a href="https://en.wikipedia.org/wiki/Project_Hail_Mary_(film)">Project Hail Mary</a>, and finished watching <a href="https://lotr.fandom.com/wiki/The_Lord_of_the_Rings_Extended_Edition">Lord of The Rings (Special Extended Edition)</a> with all bonus materials.</li>
         </ul>
         <p>This is a <a href="https://nownownow.com/about">now page</a> inspired by <a href="https://sive.rs/">Derek Sivers</a></p>
     </article>
