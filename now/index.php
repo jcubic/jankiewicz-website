5e5e5b83507e72b3ab13f9b69ef7c89a8b0f21d0
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-08-20 17:52:47 +0200

    update /now page

--- now/index.php
+++ now/index.php
@@ -15,13 +15,13 @@
 <body id="tutorials">
     <?php include('../header.php'); ?>
     <article>
-        <h2>What I'm doing now (July 2026)</h2>
+        <h2>What I'm doing now (August 2026)</h2>
         <ul>
-          <li><strong>Working</strong>: I'm still looking for a job as a Senior Software Engineer (Front-End focused, but thinking about pivoting to Full-stack with Python and AI), working on my <a href="https://github.com/jcubic">Open Source projects</a>, mentoring people, volunteering for Polish Wikipedia (where I'm a trainer and a mentor for newcomers), and moderating Twitter communites. Continue working on <a href="https://jcubic.pl/wikizeit/oferta/">paid support for Wikipedia and Wikidata</a> and my <a href="https://wikizeit.edu.pl/">WikiZEIT project</a>, where I teach about Wikipedia and SEO (Polish only)</li>
-          <li><strong>Writing</strong>: Wrote a few articles on FreeCodeCamp, WikiZEIT, and my personal blog (after a year of silence).</li>
-          <li><strong>Reading</strong>: Finished reading Polish book about personal brand by Ewelina Podrez-Siama. Started studying (after few months break) statistcs, Python and data science from books "Head First Statistcs", "Python for Data Analysis", "Essential Math for Data Science", and "Fluent Python"</li>
+          <li><strong>Working</strong>: I'm still looking for a job as a Senior Software Engineer (Front-End focused, but thinking about pivoting to Full-stack with Python and AI), working on my <a href="https://github.com/jcubic">Open Source projects</a> (released a new major beta version of <a href="https://lips.js.org/blog/beta-continuations-and-tco">LIPS Scheme</a>), volunteering for Polish Wikipedia (where I'm a trainer and a mentor for newcomers), and moderating Twitter communites. From time to doing paid edits and consultations for <a href="https://wikizeit.edu.pl/">WikiZEIT project</a>.</li>
+          <li><strong>Writing</strong>: Wrote two articles for <a href="https://lips.js.org/blog">LIPS Scheme blog</a>.</li>
+          <li><strong>Reading</strong>: Continue studying statistcs, Python and data science from books "Head First Statistcs", "Python for Data Analysis", "Essential Math for Data Science", and "Fluent Python".</li>
           <li><strong>Listening</strong>: recent bands I've seen live were <a href="https://en.wikipedia.org/wiki/System_of_a_Down">"System of A Down"</a> and <a href="https://en.wikipedia.org/wiki/In_Flames">"In Flames"</a>. Also found and enjoyed <a href="https://archive.org/details/woodstock-99-deluxe-VHS">Wootstook 99 recording from Archive.org</a>.</li>
-          <li><strong>Watching</strong>: the last movie was <a href="https://en.wikipedia.org/wiki/The_Odyssey_(2026_film)">The Odyssey</a> and finished watching <a href="https://lotr.fandom.com/wiki/The_Lord_of_the_Rings_Extended_Edition">Lord of The Rings (Special Extended Edition)</a> with all bonus materials.</li>
+          <li><strong>Watching</strong>: the last movie was <a href="https://en.wikipedia.org/wiki/The_Odyssey_(2026_film)">The Odyssey</a> and finished watching <a href="https://lotr.fandom.com/wiki/The_Lord_of_the_Rings_Extended_Edition">Lord of The Rings (Special Extended Edition)</a> with all bonus materials. And watched few episodes of 2nd season of the <a href="https://en.wikipedia.org/wiki/1670_(TV_series)">1670</a> TV series.</li>
         </ul>
         <p>This is a <a href="https://nownownow.com/about">now page</a> inspired by <a href="https://sive.rs/">Derek Sivers</a></p>
     </article>
