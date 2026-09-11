1523fe7b8f5d82ce793380018a61626ba8a6688e
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-09-11 12:30:40 +0200

    update /now page

--- now/index.php
+++ now/index.php
@@ -15,13 +15,13 @@
 <body id="tutorials">
     <?php include('../header.php'); ?>
     <article>
-        <h2>What I'm doing now (August 2026)</h2>
+        <h2>What I'm doing now (September 2026)</h2>
         <ul>
-          <li><strong>Working</strong>: I'm still looking for a job as a Senior Software Engineer (Front-End focused, but thinking about pivoting to Full-stack with Python and AI), working on my <a href="https://github.com/jcubic">Open Source projects</a> (released a new major beta version of <a href="https://lips.js.org/blog/beta-continuations-and-tco">LIPS Scheme</a>), volunteering for Polish Wikipedia (where I'm a trainer and a mentor for newcomers), and moderating Twitter communites. From time to doing paid edits and consultations for <a href="https://wikizeit.edu.pl/">WikiZEIT project</a>.</li>
-          <li><strong>Writing</strong>: Wrote two articles for <a href="https://lips.js.org/blog">LIPS Scheme blog</a>.</li>
-          <li><strong>Reading</strong>: Continue studying statistcs, Python and data science from books "Head First Statistcs", "Python for Data Analysis", "Essential Math for Data Science", and "Fluent Python".</li>
-          <li><strong>Listening</strong>: recent bands I've seen live were <a href="https://en.wikipedia.org/wiki/System_of_a_Down">"System of A Down"</a> and <a href="https://en.wikipedia.org/wiki/In_Flames">"In Flames"</a>. Also found and enjoyed <a href="https://archive.org/details/woodstock-99-deluxe-VHS">Wootstook 99 recording from Archive.org</a>.</li>
-          <li><strong>Watching</strong>: the last movie was <a href="https://en.wikipedia.org/wiki/The_Odyssey_(2026_film)">The Odyssey</a> and finished watching <a href="https://lotr.fandom.com/wiki/The_Lord_of_the_Rings_Extended_Edition">Lord of The Rings (Special Extended Edition)</a> with all bonus materials. And watched few episodes of 2nd season of the <a href="https://en.wikipedia.org/wiki/1670_(TV_series)">1670</a> TV series.</li>
+          <li><strong>Working</strong>: I'm still looking for a job as a Senior Software Engineer (Front-End focused, but thinking about pivoting to Full-stack with Python and AI), working on my <a href="https://github.com/jcubic">Open Source projects</a> started working on <a href="https://hacking.cafe">hacking.cafe</a> terminal, volunteering for Polish Wikipedia, and moderating Twitter communites. From time to doing paid edits and consultations for <a href="https://wikizeit.edu.pl/">WikiZEIT project</a>.</li>
+          <li><strong>Writing</strong>: Wrote two articles for <a href="https://wikizeit.edu.pl/blog">WikiZEIT blog</a>.</li>
+          <li><strong>Reading</strong>: Started reading "A Philosophy of Software Design" by <a href="https://en.wikipedia.org/wiki/John_Ousterhout">John Ousterhout</a>.</li>
+          <li><strong>Listening</strong>: varous metal adn rock bands, but I keep navigating thowards my mp3 collection from Spotify and YouTube.</li>
+          <li><strong>Watching</strong>: the last movie was <a href="https://en.wikipedia.org/wiki/Mad_Max_(film)">Mad Max</a> (original). And finished watching 2nd season of the <a href="https://en.wikipedia.org/wiki/1670_(TV_series)">1670</a> TV series.</li>
         </ul>
         <p>This is a <a href="https://nownownow.com/about">now page</a> inspired by <a href="https://sive.rs/">Derek Sivers</a></p>
     </article>
